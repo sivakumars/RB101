0c64a42be11756b52cca279d0765b966bf675703
sivakumars <5514983+sivakumars@users.noreply.github.com>
2021-11-02 09:23:01 +0530

    Renaming the prompt method

--- mortgage_calculator.rb
+++ mortgage_calculator.rb
@@ -20,33 +20,33 @@ def positive?(number)
   number.to_f > 0
 end
 
-def prompt_message(message)
+def prompt(message)
   puts(">> #{message}")
 end
 
 loop do
   loan_amount = ''
   loop do
-    prompt_message("Enter a valid loan amount: ")
+    prompt("Enter a valid loan amount: ")
     loan_amount = gets.chomp
     break if valid_number?(loan_amount) && positive?(loan_amount)
-    prompt_message("Invalid number! Please enter a valid loan amount")
+    prompt("Invalid number! Please enter a valid loan amount")
   end
 
   annual_percentage_rate = ''
   loop do
-    prompt_message("Enter the annual percentage rate. e.g., 10 for 10%; 10.5 for 10.5% : ")
+    prompt("Enter the annual percentage rate. e.g., 10 for 10%; 10.5 for 10.5% : ")
     annual_percentage_rate = gets.chomp
     break if valid_number?(annual_percentage_rate) && positive?(annual_percentage_rate)
-    prompt_message("Invalid percentage rate. Please enter a valid rate")
+    prompt("Invalid percentage rate. Please enter a valid rate")
   end
 
   loan_duration = ''
   loop do
-    prompt_message("Enter the loan duration in years: ")
+    prompt("Enter the loan duration in years: ")
     loan_duration = gets.chomp
     break if valid_number?(loan_duration) && positive?(loan_duration)
-    prompt_message("Invalid loan duration. Please enter a valid duration")
+    prompt("Invalid loan duration. Please enter a valid duration")
   end
 
   puts "#{loan_amount} #{annual_percentage_rate} #{loan_duration}"
@@ -60,7 +60,7 @@ loop do
 
   puts "Monthly payment rate: #{monthly_payment_rate.round(2)}"
 
-  prompt_message("Do you want to calculate again? Enter Y to calculate again and any key to exit: ")
+  prompt("Do you want to calculate again? Enter Y to calculate again and any key to exit: ")
   repeat_calculation = gets.chomp
 
   break unless repeat_calculation.downcase.start_with?('y')
