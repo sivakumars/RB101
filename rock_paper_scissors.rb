bbf5b3f397c15b3c2cced14719b6fe97d880456c
sivakumars <5514983+sivakumars@users.noreply.github.com>
2021-11-03 10:17:46 +0530

    RPSSL bonus features

--- rock_paper_scissors.rb
+++ rock_paper_scissors.rb
@@ -4,52 +4,116 @@
   # Assign a computer choice
   # compare the user choice with the computer choice
   # display results
+  Lizard Spock
+
+Typing the full word "rock" or "lizard" is tiring. 
+Update the program so the user can type "r" for "rock," "p" for "paper," etc. 
+Note that if you do bonus #1, you'll have two words that start with "s." How do you resolve that?
+
+Keep score of the player's and computer's wins. 
+When either the player or computer reaches three wins, the match is over, and the winning player becomes the grand winner. 
+Don't add your incrementing logic to display_results. Keep your methods simple; 
+they should perform one logical task — no more, no less.
 =end
-VALID_CHOICES = ['rock', 'paper', 'scissors']
+VALID_CHOICES = {
+  r: 'rock',
+  p: 'paper',
+  sc: 'scissors',
+  sp: 'spock',
+  l: 'lizard'
+}
 
-def test_method(param)
-  prompt("This is a test method #{param}")
+$player_stats = {
+  user: 0,
+  computer: 0
+}
+
+def print_text(message, prompt = false)
+  prompt ?  puts(">> #{message}") : puts("\n#{message}")
 end
 
-# test_method("before")
-def prompt(message)
-  puts ">> #{message}"
+def display_wait
+  print "wait...."
+  sleep(1)
 end
 
-def win?(player_one, player_second)
-  (player_one == 'rock' && player_second == 'scissors') ||
-    (player_one == 'paper' && player_second == 'rock') ||
-    (player_one == 'scissors' && player_second == 'paper')
+def display_choices
+  print_text("Available choices: ")
+  VALID_CHOICES.each_with_index{ |(key, value), index| puts " #{index+1}.#{key} (#{value})"}
+end
+
+def valid_choice?(choice)
+  VALID_CHOICES.keys.include?(:"#{choice}")
+end
+
+def get_symbol_value(key)
+  VALID_CHOICES[key.to_sym]
+end
+
+def update_wins(key)
+  $player_stats[key.to_sym] += 1
+end
+
+def winner?(first, second, player)
+  winner = case first
+           when 'rock'
+            ['scissors', 'lizard'].include?(second)
+           when 'paper'
+            ['rock', 'spock'].include?(second)
+           when 'scissors'
+             ['paper', 'lizard'].include?(second)
+           when 'spock'
+             ['rock', 'scissors'].include?(second)
+           when 'lizard'
+             ['paper', 'spock'].include?(second)
+           end
+  update_wins(player) if winner
+  winner
 end
 
 def display_results(player, computer)
-  if win?(player, computer)
-    prompt('You won!')
-  elsif win?(computer, player)
-    prompt('Computer won :(')
+  display_wait
+  if winner?(player, computer, 'user')
+    print_text('You won!')
+  elsif winner?(computer, player, 'computer')
+    print_text('Computer won :(')
   else
-    prompt("It's a tie")
+    print_text("It's a tie")
   end
 end
 
 loop do
-  user_choice = ''
-  loop do
-    prompt("Make a choice: #{VALID_CHOICES.join(', ')}")
-    user_choice = gets.chomp
-    break if VALID_CHOICES.include?(user_choice)
-    prompt("You entered an invalid choice")
+  display_choices
+  while $player_stats.values.max < 3
+    print_text("Make a choice: ", true)
+    
+    user_choice = ''
+    loop do
+      user_choice = gets.chomp
+      break if valid_choice?(user_choice)
+      print_text("You entered an invalid choice")
+    end
+
+    computer_choice = VALID_CHOICES.keys.sample
+
+    user_choice_value = get_symbol_value(user_choice) || ""
+    computer_choice_value = get_symbol_value(computer_choice) || ""
+
+    print_text("Your choice: #{user_choice_value}; Computer's choice: #{computer_choice_value}")
+    display_results(user_choice_value,computer_choice_value)
   end
 
-  computer_choice = VALID_CHOICES.sample
-
-  puts "Your choice: #{user_choice}; Computer's choice: #{computer_choice}"
-
-  display_results(user_choice, computer_choice)
-
-  prompt("Do you want to play again? Press 'y' to play again ")
+  grand_winner = $player_stats.key($player_stats.values.max).to_s
+  print_text("The grand winner is: #{grand_winner.capitalize!}")
+  
+  print_text("Do you want to play again? Press 'y' to play again ", true)
   play_again = gets.chomp
-  break unless play_again.downcase.start_with?('y')
+  if play_again.downcase.start_with?('y')
+    $player_stats.each{|key, value| $player_stats[key] = 0}
+    system("cls") || system("clear")
+  else
+    break
+  end
 end
 
-prompt("Thank you for playing!")
+print_text("Thank you for playing!")
