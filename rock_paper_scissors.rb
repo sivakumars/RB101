000cb706f1baf39639aafef7655efb3e37df43e9
sivakumars <5514983+sivakumars@users.noreply.github.com>
2021-11-03 13:07:20 +0530

    Removing global variables and passing local variables as arguments to update player stats

--- rock_paper_scissors.rb
+++ rock_paper_scissors.rb
@@ -10,13 +10,8 @@ VALID_CHOICES = {
 
 MAX_WINS = 3
 
-$player_stats = {
-  user: 0,
-  computer: 0
-}
-
 def print_text(message, prompt = false)
-  prompt ?  puts(">> #{message}") : puts(message)
+  prompt ? puts(">> #{message}") : puts(message)
 end
 
 def display_wait
@@ -43,11 +38,15 @@ def get_symbol_value(key)
   VALID_CHOICES[key.to_sym]
 end
 
-def update_wins(key)
-  $player_stats[key.to_sym] += 1
+def update_wins(key, stats)
+  stats[key.to_sym] += 1
 end
 
-def winner?(first, second, player)
+def reset(stats_hash)
+  stats_hash.each { |key, _| stats_hash[key] = 0 }
+end
+
+def winner?(first, second, player, stats)
   winner = case first
            when 'rock'
              ['scissors', 'lizard'].include?(second)
@@ -60,35 +59,39 @@ def winner?(first, second, player)
            when 'lizard'
              ['paper', 'spock'].include?(second)
            end
-  update_wins(player) if winner
+  update_wins(player, stats) if winner
   winner
 end
 
-def display_results(player, computer)
-  if winner?(player, computer, 'user')
-    print_text("+++ #{$user_name} won! +++")
-  elsif winner?(computer, player, 'computer')
+def display_results(player, computer, stats, name)
+  if winner?(player, computer, 'user', stats)
+    print_text("+++ #{name} won! +++")
+  elsif winner?(computer, player, 'computer', stats)
     print_text('+++ Computer won :( +++')
   else
     print_text("+++ It's a tie +++")
   end
 end
 
-$user_name = ''
+user_name = ''
 loop do
   print_text("Enter your name: ")
-  $user_name = gets.chomp
-  break if $user_name.length > 2 && $user_name.match(/[a-zA-Z]/)
+  user_name = gets.chomp
+  break if user_name.length > 2 && user_name.match(/[a-zA-Z]/)
   print_text("Please enter a valid name atleast 3 characters long")
 end
-$user_name.capitalize!
+user_name.capitalize!
 
+player_stats = {
+  user: 0,
+  computer: 0
+}
 loop do
   display_choices_list
-  while $player_stats.values.max < MAX_WINS
+  while player_stats.values.max < MAX_WINS
     user_choice = ''
     loop do
-      print_text("#{$user_name}, Make a choice(#{choices_keys}): ", true)
+      print_text("#{user_name}, Make a choice(#{choices_keys}): ", true)
       user_choice = gets.chomp
       break if valid_choice?(user_choice)
       print_text("You entered an invalid choice")
@@ -100,24 +103,27 @@ loop do
     computer_choice_value = get_symbol_value(computer_choice) || "<No value>"
 
     print_text("********************************************")
-    print_text("#{$user_name}'s choice: #{user_choice_value}")
+    print_text("#{user_name}'s choice: #{user_choice_value}")
     print_text("Computer's choice: #{computer_choice_value}")
     print_text("********************************************")
 
     display_wait
-    display_results(user_choice_value, computer_choice_value)
+    display_results(user_choice_value,
+                    computer_choice_value,
+                    player_stats,
+                    user_name)
   end
 
-  grand_winner = $player_stats.key($player_stats.values.max).to_s
+  grand_winner = player_stats.key(player_stats.values.max).to_s
   if grand_winner == 'user'
-    grand_winner = $user_name
+    grand_winner = user_name
   end
   print_text("The grand winner is: #{grand_winner.upcase}")
 
   print_text("Do you want to play again? Press 'y' to play again ", true)
   play_again = gets.chomp
   if play_again.downcase.start_with?('y')
-    $player_stats.each { |key, _| $player_stats[key] = 0 }
+    reset(player_stats)
     system("cls") || system("clear")
   else
     break
